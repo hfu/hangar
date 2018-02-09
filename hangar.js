5646e182c0b53b5213823aab9d74570d530595a1
Hidenori Fujimura <hfu@users.noreply.github.com>
2018-02-09 19:29:40 +0000

    update

--- hangar.js
+++ hangar.js
@@ -7,30 +7,40 @@ const params = {
 }
 
 new MBTiles(params.dst, (err, mbtiles) => {
-  mbtiles.startWriting(err => {
-    if (err) throw err
+  if (err) throw err
+  //mbtiles.startWriting(err => {
+    //if (err) throw err
     for(let z = 0; z <= 24; z++) {
       fs.readdir(`${params.src}/${z}`, (err, items) => {
         if (err) return
-        for(const xs of items) {
+        items.slice(0, 1).forEach(xs => {
           const x = Number(xs)
           fs.readdir(`${params.src}/${z}/${x}`, (err, items) => {
             if (err) return
-            for(const fn of items) {
+            items.slice(0, 2).forEach(fn => {
               if (fn.endsWith(params.ext)) {
                 const y = Number(fn.replace(`.${params.ext}`, ''))
                 const path = `${params.src}/${z}/${x}/${fn}`
                 const buf = fs.readFileSync(path, {encoding: 'utf-8'})
                 const gz = zlib.gzipSync(buf)
                 console.log(`${z}/${x}/${y} ${buf.length} => ${gz.length} (${Math.round(100.0 * gz.length / buf.length)}%)`)
-                mbtiles.putTile(z, x, y, gz, err => {
-                  if (err) console.log(err)
+                mbtiles.startWriting(err => {
+                  console.log('started')
+                  if (err) throw err
+                  mbtiles.putTile(z, x, y, gz, err => {
+                    console.log('put')
+                    if (err) console.log(err)
+                    mbtiles.stopWriting(err => {
+                      if (err) throw err
+                      console.log('stopped')
+                    })
+                  })
                 })
               }
-            }
+            })
           })
-        }
+        })
       })
     }
-  })
+  //})
 })
